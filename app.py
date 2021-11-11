cf23fafac0a715f1b9eca44387c4e9c2eb6906ac
Liam Thomas-Chollier <90798991+LiamTHCH@users.noreply.github.com>
2021-11-11 01:32:44 +0100

    class et ajout info
    changement en class et ajout de mail, telephone, universite et mulituser
     mais le telephone n'est pas encore ascocier au user

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 import requests
 from lxml import html
-user = 'hilt'
 url = "https://annuaire.uha.fr/index.php"
-response = requests.post(url, allow_redirects=False, data={
-		'search': user,
-		'action': 'Chercher'
-	})
-#print (response.content)
-tree = html.fromstring(response.content)
-nom = tree.xpath('//span[@class="majuscules"]/text()')
-prenom, uni = tree.xpath('//span[@class="gras"]/text()')
+class anuaire:
+	def __init__(self, url):
+		self.name = str()
+		self.tel = str()
+		self.nom = str()
+		self.prenom = str()
+		self.uni = str()
+		self.mail = str()
+		self.tree = 0
+	def search(self,name):
+		user = name
+		response = requests.post(url, allow_redirects=False, data={
+				'search': user,
+				'action': 'Chercher'
+			})
+		#print (response.content)
+		self.tree = html.fromstring(response.content)
+		self.nom = self.tree.xpath('//span[@class="majuscules"]/text()')
+		self.prenom = self.tree.xpath('//span[@class="gras"]/text()')
+		del self.prenom[-1]
+		self.uni = test = self.tree.xpath('//div[@class="xl-col-6 l-col-6 m-col-6 ml-col-6 s-col-12 sl-col-12"]/p/text()')
+		self.mail = test = self.tree.xpath('//div[@class="xl-col-6 l-col-6 m-col-6 ml-col-6 s-col-12 sl-col-12 droite"]/p/a/text()')
+		self.tel = self.tree.xpath('//div[@class="xl-col-6 l-col-6 m-col-6 ml-col-6 s-col-12 sl-col-12 droite"]/p/span/a/text()')
+
+		#numero pas ascocier a l'utilisateur
