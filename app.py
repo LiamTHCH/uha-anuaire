05808cf4d01facaf0c6ef5919851bab97a353856
Liam Thomas-Chollier <90798991+LiamTHCH@users.noreply.github.com>
2021-11-11 01:01:41 +0100

    ajout du nom,prenom et universite

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import requests
+from lxml import html
 user = 'hilt'
 url = "https://annuaire.uha.fr/index.php"
 response = requests.post(url, allow_redirects=False, data={
 		'search': user,
 		'action': 'Chercher'
 	})
-print (response.content)
+#print (response.content)
+tree = html.fromstring(response.content)
+nom = tree.xpath('//span[@class="majuscules"]/text()')
+prenom, uni = tree.xpath('//span[@class="gras"]/text()')
