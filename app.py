be2650c118716a26808f5458419a262ba3b10b7f
Liam Thomas-Chollier <90798991+LiamTHCH@users.noreply.github.com>
2022-01-05 21:19:01 +0100

    mise a jour pour le minitel et eleve
    Signed-off-by: Liam Thomas-Chollier <90798991+LiamTHCH@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+from _typeshed import Self
 import requests
 from lxml import html
 url = "https://annuaire.uha.fr/index.php"
+cookies = {'Annuaire': 'ST-289323-4pnACrW6JoNR5Mz1Smv-LEfnSvA-cas6uhafr'}
 class anuaire:
 	def __init__(self, url):
 		self.name = str()
@@ -10,18 +12,18 @@ class anuaire:
 		self.uni = str()
 		self.mail = str()
 		self.name = str()
-		self.tels = []
-		self.noms = []
-		self.prenoms = []
-		self.unis = []
-		self.mails = []
 		self.tree = 0
-	def search(self,name):
+		self.listeleve = []
+		self.listprof = []
+	def search(self,name,type):
 		user = name
+		if type == "prof" : requests.get("https://annuaire.uha.fr/index.php?type=pers",cookies=cookies)
+		elif type == "eleve" : requests.get("https://annuaire.uha.fr/index.php?type=etud",cookies=cookies)
+		else: exit(-1)
 		response = requests.post(url, allow_redirects=False, data={
 				'search': user,
 				'action': 'Chercher'
-			})
+			}, cookies=cookies)
 		#print (response.content)
 		self.tree = html.fromstring(response.content)
 		self.nom = self.tree.xpath('//span[@class="majuscules"]/text()')
@@ -30,31 +32,36 @@ class anuaire:
 		self.uni = test = self.tree.xpath('//div[@class="xl-col-6 l-col-6 m-col-6 ml-col-6 s-col-12 sl-col-12"]/p/text()')
 		self.mail = test = self.tree.xpath('//div[@class="xl-col-6 l-col-6 m-col-6 ml-col-6 s-col-12 sl-col-12 droite"]/p/a/text()')
 		self.tel = self.tree.xpath('//div[@class="xl-col-6 l-col-6 m-col-6 ml-col-6 s-col-12 sl-col-12 droite"]/p/span/a/text()')
-
-		#numero pas ascocier a l'utilisateur
-	def searchglobal(self,term):
-		user = term
+		return dict(nom=self.nom + self.prenom, adresse=self.mail,cp="", ville=self.uni, tel=self.tel.string)
+	
+	def getlist(self,term):
+		requests.get("https://annuaire.uha.fr/index.php?type=pers",cookies=cookies)
 		response = requests.post(url, allow_redirects=False, data={
 				'search': user,
 				'action': 'Chercher'
-			})
+			}, cookies=cookies)
 		self.tree = html.fromstring(response.content)
-		self.noms = self.tree.xpath('//span[@class="majuscules"]/text()')
-		self.prenoms = self.tree.xpath('//span[@class="gras"]/text()')
-		del self.prenoms[-1]
-		for i in range(0,len(self.noms)):
-			self.search(self.prenoms[i]+" "+self.noms[i])
-			if not self.tel:
-				self.tels.append("")
-			else:
-				self.tels.append(self.tel[0])
-			if not self.uni :
-				self.unis.append("")
-			else:
-				self.unis.append(self.uni[0])
-			if not self.mail:
-				self.mails.append("")
-			else:
-				self.mails.append(self.mail[0])
+		self.nom = self.tree.xpath('//span[@class="majuscules"]/text()')
+		self.prenom = self.tree.xpath('//span[@class="gras"]/text()')
+		self.listprof.append(self.nom + self.prenom)
+		requests.get("https://annuaire.uha.fr/index.php?type=etud",cookies=cookies)
+		response = requests.post(url, allow_redirects=False, data={
+				'search': user,
+				'action': 'Chercher'
+			}, cookies=cookies)
+		self.tree = html.fromstring(response.content)
+		self.nom = self.tree.xpath('//span[@class="majuscules"]/text()')
+		self.prenom = self.tree.xpath('//span[@class="gras"]/text()')
+		self.listeleve.append(self.nom + self.prenom)
+	def searchglobal(self,nom):
+		res = []
+		self.getlist(nom)
+		for user in self.listprof:
+			res.append(self.search(user))
+		for user in self.listeleve:
+			res.append(self.search(user))
+		return res
+
+
 
 
